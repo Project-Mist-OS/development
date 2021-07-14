android/platform_development

Extend header-abi-dumper/linker --root-dir
This commit extends the format of --root-dir to <path>:<replacement>. If
a source path starts with <path>, the prefix is replaced. The build
system can use this option to have the ABI tools replace non-default
OUT_DIR with "out". This commit also allows more than one --root-dir.
When the tool normalizes a source path, the tool finds the longest
matching root dir.

Test: ./header-checker-unittests
Bug: 192157880
Change-Id: Ie77b4f09304e029aa5c2acc6a000c59400913426

vndk/tools/header-checker/src/utils/source_path_utils.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "utils/header_abi_util.h"
+#include "utils/source_path_utils.h"
 
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/Path.h>
@@ -37,33 +37,82 @@ static bool ShouldSkipFile(llvm::StringRef &file_name) {
           file_name.endswith(".cc") || file_name.endswith(".c"));
 }
 
-std::string GetCwd() {
+static std::string GetCwd() {
   llvm::SmallString<256> cwd;
   if (llvm::sys::fs::current_path(cwd)) {
     llvm::errs() << "ERROR: Failed to get current working directory\n";
     ::exit(1);
   }
-  return cwd.c_str();
+  return std::string(cwd);
 }
 
-std::string NormalizePath(const std::string &path,
-                          const std::string &root_dir) {
-  llvm::SmallString<256> norm_path(path);
+RootDirs ParseRootDirs(const std::vector<std::string> &args) {
+  const RootDirs no_root_dirs;
+  RootDirs root_dirs;
+  for (const std::string_view arg : args) {
+    std::string_view path;
+    std::string_view replacement;
+    size_t colon_index = arg.find(":");
+    if (colon_index != std::string_view::npos) {
+      path = arg.substr(0, colon_index);
+      replacement = arg.substr(colon_index + 1);
+    } else {
+      path = arg;
+      replacement = "";
+    }
+    llvm::SmallString<256> norm_replacement(replacement.begin(),
+                                            replacement.end());
+    llvm::sys::path::remove_dots(norm_replacement, /* remove_dot_dot = */ true);
+    root_dirs.emplace_back(NormalizePath(path, no_root_dirs),
+                           std::string(norm_replacement));
+  }
+  if (root_dirs.empty()) {
+    root_dirs.emplace_back(GetCwd(), "");
+  }
+  // Sort by length in descending order so that NormalizePath finds the longest
+  // matching root dir.
+  std::sort(root_dirs.begin(), root_dirs.end(),
+            [](RootDir &first, RootDir &second) {
+              return first.path_.size() > second.path_.size();
+            });
+  return root_dirs;
+}
+
+std::string NormalizePath(std::string_view path, const RootDirs &root_dirs) {
+  llvm::SmallString<256> norm_path(path.begin(), path.end());
   if (llvm::sys::fs::make_absolute(norm_path)) {
     return "";
   }
   llvm::sys::path::remove_dots(norm_path, /* remove_dot_dot = */ true);
-  // Convert /cwd/path to /path.
-  if (llvm::sys::path::replace_path_prefix(norm_path, root_dir, "")) {
-    // Convert /path to path.
-    return llvm::sys::path::relative_path(norm_path.str()).str();
+  llvm::StringRef separator = llvm::sys::path::get_separator();
+  // Convert /root/dir/path to path.
+  for (const RootDir &root_dir : root_dirs) {
+    // llvm::sys::path::replace_path_prefix("AB", "A", "") returns "B", so do
+    // not use it.
+    if (!norm_path.startswith(root_dir.path_)) {
+      continue;
+    }
+    if (norm_path.size() == root_dir.path_.size()) {
+      return root_dir.replacement_;
+    }
+    llvm::StringRef suffix = norm_path.substr(root_dir.path_.size());
+    if (suffix.startswith(separator)) {
+      if (root_dir.replacement_.empty()) {
+        return suffix.substr(separator.size()).str();
+      }
+      // replacement == "/"
+      if (llvm::StringRef(root_dir.replacement_).endswith(separator)) {
+        return root_dir.replacement_ + suffix.substr(separator.size()).str();
+      }
+      return root_dir.replacement_ + suffix.str();
+    }
   }
   return std::string(norm_path);
 }
 
 static bool CollectExportedHeaderSet(const std::string &dir_name,
                                      std::set<std::string> *exported_headers,
-                                     const std::string &root_dir) {
+                                     const RootDirs &root_dirs) {
   std::error_code ec;
   llvm::sys::fs::recursive_directory_iterator walker(dir_name, ec);
   // Default construction - end of directory.
@@ -98,17 +147,17 @@ static bool CollectExportedHeaderSet(const std::string &dir_name,
       continue;
     }
 
-    exported_headers->insert(NormalizePath(file_path, root_dir));
+    exported_headers->insert(NormalizePath(file_path, root_dirs));
   }
   return true;
 }
 
 std::set<std::string>
 CollectAllExportedHeaders(const std::vector<std::string> &exported_header_dirs,
-                          const std::string &root_dir) {
+                          const RootDirs &root_dirs) {
   std::set<std::string> exported_headers;
   for (auto &&dir : exported_header_dirs) {
-    if (!CollectExportedHeaderSet(dir, &exported_headers, root_dir)) {
+    if (!CollectExportedHeaderSet(dir, &exported_headers, root_dirs)) {
       llvm::errs() << "Couldn't collect exported headers\n";
       ::exit(1);
     }

vndk/tools/header-checker/src/utils/source_path_utils.h
@@ -1,0 +1,52 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SOURCE_PATH_UTILS_H_
+#define SOURCE_PATH_UTILS_H_
+
+#include <set>
+#include <string>
+#include <vector>
+
+
+namespace header_checker {
+namespace utils {
+
+
+struct RootDir {
+  std::string path_;
+  std::string replacement_;
+
+  RootDir(std::string path, std::string replacement)
+      : path_(std::move(path)), replacement_(std::move(replacement)) {}
+};
+
+typedef std::vector<RootDir> RootDirs;
+
+RootDirs ParseRootDirs(const std::vector<std::string> &args);
+
+// Resolve '..' and '.'; if the path starts with any of root_dirs, replace the
+// prefix; don't resolve symbolic links.
+std::string NormalizePath(std::string_view path, const RootDirs &root_dirs);
+
+std::set<std::string>
+CollectAllExportedHeaders(const std::vector<std::string> &exported_header_dirs,
+                          const RootDirs &root_dirs);
+
+
+}  // namespace utils
+}  // namespace header_checker
+
+
+#endif  // SOURCE_PATH_UTILS_H_

vndk/tools/header-checker/src/utils/source_path_utils_test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "utils/header_abi_util.h"
+#include "utils/source_path_utils.h"
 
 #include <gtest/gtest.h>
 
@@ -21,23 +21,57 @@ namespace header_checker {
 namespace utils {
 
 
-TEST(CollectExportedHeadersTest, NormalizeAbsolutePaths) {
-  const std::string root = "/root/dir";
-  EXPECT_EQ("", NormalizePath(root, root));
-  EXPECT_EQ("/unit/test", NormalizePath("/unit/test", root));
-  EXPECT_EQ("/root/unit/test", NormalizePath(root + "/../unit/test", root));
+TEST(SourcePathUtilsTest, NormalizeAbsolutePaths) {
+  const std::vector<std::string> args{"/root/dir"};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(1, root_dirs.size());
+  ASSERT_EQ("/root/dir", root_dirs[0].path_);
+  ASSERT_EQ("", root_dirs[0].replacement_);
+
+  EXPECT_EQ("", NormalizePath("/root/dir", root_dirs));
+  EXPECT_EQ("test", NormalizePath("/root/dir/test", root_dirs));
+  EXPECT_EQ("/root/unit/test",
+            NormalizePath("/root/dir/../unit/test", root_dirs));
 }
 
 
-TEST(CollectExportedHeadersTest, NormalizeCwdPaths) {
-  const std::string cwd = GetCwd();
-  ASSERT_NE("", cwd);
+TEST(SourcePathUtilsTest, NormalizeCwdPaths) {
+  const RootDirs cwd = ParseRootDirs(std::vector<std::string>());
+  ASSERT_EQ(1, cwd.size());
+  ASSERT_NE("", cwd[0].path_);
+  ASSERT_EQ("", cwd[0].replacement_);
+
   EXPECT_EQ("", NormalizePath("", cwd));
   EXPECT_EQ("unit/test", NormalizePath("./unit/test/.", cwd));
   EXPECT_EQ("unit/test", NormalizePath("unit//test//", cwd));
   EXPECT_EQ("test", NormalizePath("unit/../test", cwd));
-  EXPECT_EQ("unit/test", NormalizePath(cwd + "/unit/test", cwd));
+  EXPECT_EQ("unit/test", NormalizePath(cwd[0].path_ + "/unit/test", cwd));
   EXPECT_EQ('/', NormalizePath("../unit/test", cwd)[0]);
+}
+
+
+TEST(SourcePathUtilsTest, NormalizePathsWithMultipleRootDirs) {
+  const std::vector<std::string> args{"/before:/", "/before/dir:after"};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(2, root_dirs.size());
+  ASSERT_EQ("/before/dir", root_dirs[0].path_);
+  ASSERT_EQ("after", root_dirs[0].replacement_);
+  ASSERT_EQ("/before", root_dirs[1].path_);
+  ASSERT_EQ("/", root_dirs[1].replacement_);
+
+  EXPECT_EQ("/directory", NormalizePath("/before/directory", root_dirs));
+  EXPECT_EQ("after", NormalizePath("/before/dir", root_dirs));
+}
+
+
+TEST(SourcePathUtilsTest, NormalizeRelativePaths) {
+  const std::vector<std::string> args{"../before/.:..//after/."};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(1, root_dirs.size());
+  ASSERT_EQ('/', root_dirs[0].path_[0]);
+  ASSERT_EQ("../after", root_dirs[0].replacement_);
+
+  EXPECT_EQ("../after", NormalizePath("../before", root_dirs));
 }
 
 

Project-Mist-OS/development

vndk/tools/header-checker/src/utils/source_path_utils.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "utils/header_abi_util.h"
+#include "utils/source_path_utils.h"
 
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/Path.h>
@@ -37,33 +37,87 @@ static bool ShouldSkipFile(llvm::StringRef &file_name) {
           file_name.endswith(".cc") || file_name.endswith(".c"));
 }
 
-std::string GetCwd() {
+static std::string GetCwd() {
   llvm::SmallString<256> cwd;
   if (llvm::sys::fs::current_path(cwd)) {
     llvm::errs() << "ERROR: Failed to get current working directory\n";
     ::exit(1);
   }
-  return cwd.c_str();
+  return std::string(cwd);
 }
 
-std::string NormalizePath(const std::string &path,
-                          const std::string &root_dir) {
-  llvm::SmallString<256> norm_path(path);
+RootDirs ParseRootDirs(const std::vector<std::string> &args) {
+  RootDirs root_dirs;
+  for (const std::string_view arg : args) {
+    std::string_view path;
+    std::string_view replacement;
+    size_t colon_index = arg.find(":");
+    if (colon_index != std::string_view::npos) {
+      path = arg.substr(0, colon_index);
+      replacement = arg.substr(colon_index + 1);
+    } else {
+      path = arg;
+      replacement = "";
+    }
+    llvm::SmallString<256> norm_replacement(replacement.begin(),
+                                            replacement.end());
+    llvm::sys::path::remove_dots(norm_replacement, /* remove_dot_dot = */ true);
+    root_dirs.emplace_back(NormalizePath(path, {}),
+                           std::string(norm_replacement));
+  }
+  if (root_dirs.empty()) {
+    root_dirs.emplace_back(GetCwd(), "");
+  }
+  // Sort by length in descending order so that NormalizePath finds the longest
+  // matching root dir.
+  std::sort(root_dirs.begin(), root_dirs.end(),
+            [](RootDir &first, RootDir &second) {
+              return first.path.size() > second.path.size();
+            });
+  for (size_t index = 1; index < root_dirs.size(); index++) {
+    if (root_dirs[index - 1].path == root_dirs[index].path) {
+      llvm::errs() << "Duplicate root dir: " << root_dirs[index].path << "\n";
+      ::exit(1);
+    }
+  }
+  return root_dirs;
+}
+
+std::string NormalizePath(std::string_view path, const RootDirs &root_dirs) {
+  llvm::SmallString<256> norm_path(path.begin(), path.end());
   if (llvm::sys::fs::make_absolute(norm_path)) {
     return "";
   }
   llvm::sys::path::remove_dots(norm_path, /* remove_dot_dot = */ true);
-  // Convert /cwd/path to /path.
-  if (llvm::sys::path::replace_path_prefix(norm_path, root_dir, "")) {
-    // Convert /path to path.
-    return llvm::sys::path::relative_path(norm_path.str()).str();
+  llvm::StringRef separator = llvm::sys::path::get_separator();
+  // Convert /root/dir/path to path.
+  for (const RootDir &root_dir : root_dirs) {
+    // llvm::sys::path::replace_path_prefix("AB", "A", "") returns "B", so do
+    // not use it.
+    if (!norm_path.startswith(root_dir.path)) {
+      continue;
+    }
+    if (norm_path.size() == root_dir.path.size()) {
+      return root_dir.replacement;
+    }
+    llvm::StringRef suffix = norm_path.substr(root_dir.path.size());
+    if (suffix.startswith(separator)) {
+      if (root_dir.replacement.empty()) {
+        return suffix.substr(separator.size()).str();
+      }
+      // replacement == "/"
+      if (llvm::StringRef(root_dir.replacement).endswith(separator)) {
+        return root_dir.replacement + suffix.substr(separator.size()).str();
+      }
+      return root_dir.replacement + suffix.str();
+    }
   }
   return std::string(norm_path);
 }
 
 static bool CollectExportedHeaderSet(const std::string &dir_name,
                                      std::set<std::string> *exported_headers,
-                                     const std::string &root_dir) {
+                                     const RootDirs &root_dirs) {
   std::error_code ec;
   llvm::sys::fs::recursive_directory_iterator walker(dir_name, ec);
   // Default construction - end of directory.
@@ -98,17 +152,17 @@ static bool CollectExportedHeaderSet(const std::string &dir_name,
       continue;
     }
 
-    exported_headers->insert(NormalizePath(file_path, root_dir));
+    exported_headers->insert(NormalizePath(file_path, root_dirs));
   }
   return true;
 }
 
 std::set<std::string>
 CollectAllExportedHeaders(const std::vector<std::string> &exported_header_dirs,
-                          const std::string &root_dir) {
+                          const RootDirs &root_dirs) {
   std::set<std::string> exported_headers;
   for (auto &&dir : exported_header_dirs) {
-    if (!CollectExportedHeaderSet(dir, &exported_headers, root_dir)) {
+    if (!CollectExportedHeaderSet(dir, &exported_headers, root_dirs)) {
       llvm::errs() << "Couldn't collect exported headers\n";
       ::exit(1);
     }

vndk/tools/header-checker/src/utils/source_path_utils.h
@@ -1,0 +1,52 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SOURCE_PATH_UTILS_H_
+#define SOURCE_PATH_UTILS_H_
+
+#include <set>
+#include <string>
+#include <vector>
+
+
+namespace header_checker {
+namespace utils {
+
+
+struct RootDir {
+  std::string path;
+  std::string replacement;
+
+  RootDir(std::string p, std::string r)
+      : path(std::move(p)), replacement(std::move(r)) {}
+};
+
+typedef std::vector<RootDir> RootDirs;
+
+RootDirs ParseRootDirs(const std::vector<std::string> &args);
+
+// Resolve '..' and '.'; if the path starts with any of root_dirs, replace the
+// prefix; don't resolve symbolic links.
+std::string NormalizePath(std::string_view path, const RootDirs &root_dirs);
+
+std::set<std::string>
+CollectAllExportedHeaders(const std::vector<std::string> &exported_header_dirs,
+                          const RootDirs &root_dirs);
+
+
+}  // namespace utils
+}  // namespace header_checker
+
+
+#endif  // SOURCE_PATH_UTILS_H_

vndk/tools/header-checker/src/utils/source_path_utils_test.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "utils/header_abi_util.h"
+#include "utils/source_path_utils.h"
 
 #include <gtest/gtest.h>
 
@@ -21,23 +21,57 @@ namespace header_checker {
 namespace utils {
 
 
-TEST(CollectExportedHeadersTest, NormalizeAbsolutePaths) {
-  const std::string root = "/root/dir";
-  EXPECT_EQ("", NormalizePath(root, root));
-  EXPECT_EQ("/unit/test", NormalizePath("/unit/test", root));
-  EXPECT_EQ("/root/unit/test", NormalizePath(root + "/../unit/test", root));
+TEST(SourcePathUtilsTest, NormalizeAbsolutePaths) {
+  const std::vector<std::string> args{"/root/dir"};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(1, root_dirs.size());
+  ASSERT_EQ("/root/dir", root_dirs[0].path);
+  ASSERT_EQ("", root_dirs[0].replacement);
+
+  EXPECT_EQ("", NormalizePath("/root/dir", root_dirs));
+  EXPECT_EQ("test", NormalizePath("/root/dir/test", root_dirs));
+  EXPECT_EQ("/root/unit/test",
+            NormalizePath("/root/dir/../unit/test", root_dirs));
 }
 
 
-TEST(CollectExportedHeadersTest, NormalizeCwdPaths) {
-  const std::string cwd = GetCwd();
-  ASSERT_NE("", cwd);
+TEST(SourcePathUtilsTest, NormalizeCwdPaths) {
+  const RootDirs cwd = ParseRootDirs(std::vector<std::string>());
+  ASSERT_EQ(1, cwd.size());
+  ASSERT_NE("", cwd[0].path);
+  ASSERT_EQ("", cwd[0].replacement);
+
   EXPECT_EQ("", NormalizePath("", cwd));
   EXPECT_EQ("unit/test", NormalizePath("./unit/test/.", cwd));
   EXPECT_EQ("unit/test", NormalizePath("unit//test//", cwd));
   EXPECT_EQ("test", NormalizePath("unit/../test", cwd));
-  EXPECT_EQ("unit/test", NormalizePath(cwd + "/unit/test", cwd));
+  EXPECT_EQ("unit/test", NormalizePath(cwd[0].path + "/unit/test", cwd));
   EXPECT_EQ('/', NormalizePath("../unit/test", cwd)[0]);
+}
+
+
+TEST(SourcePathUtilsTest, NormalizePathsWithMultipleRootDirs) {
+  const std::vector<std::string> args{"/before:/", "/before/dir:after"};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(2, root_dirs.size());
+  ASSERT_EQ("/before/dir", root_dirs[0].path);
+  ASSERT_EQ("after", root_dirs[0].replacement);
+  ASSERT_EQ("/before", root_dirs[1].path);
+  ASSERT_EQ("/", root_dirs[1].replacement);
+
+  EXPECT_EQ("/directory", NormalizePath("/before/directory", root_dirs));
+  EXPECT_EQ("after", NormalizePath("/before/dir", root_dirs));
+}
+
+
+TEST(SourcePathUtilsTest, NormalizeRelativePaths) {
+  const std::vector<std::string> args{"../before/.:..//after/."};
+  const RootDirs root_dirs = ParseRootDirs(args);
+  ASSERT_EQ(1, root_dirs.size());
+  ASSERT_EQ('/', root_dirs[0].path[0]);
+  ASSERT_EQ("../after", root_dirs[0].replacement);
+
+  EXPECT_EQ("../after", NormalizePath("../before", root_dirs));
 }
 
 