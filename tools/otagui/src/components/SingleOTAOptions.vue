Madeinmisery/Keyboard

Refactor the frontend for batch generation.
Following part has been modified:

1. Add tabs for selection between single generation and batch
generation. (src/components/JobConfigure.vue)

2. Change the data structure of OTAConfiguration, now it only records
the flags. The source/target build will be provided when submit jobs.
(src/services/JobSubmission.js)

3. Seperate the OTAOptions as a single component, which only takes in
the flags for backend. The selection of source/target build will be in a
seperate component. (src/components/OTAOptions.vue,
src/components/SingleOTAOptions.vue).

4. Now the partition selection can takes in more than one build, but
only show the partition list of first one. Later on, this will be able
to show the intersection of the partition lists from all given builds.
(src/components/PartialCheckbox.vue)

Point 1 enables the possibility of the dynamical loading of single/batch
ota generation pages. Point 2,3,4 allow the OTAOptions components to
be reused for batch generation.

Test: Mannual tested.
Change-Id: I1a29fa7c605596d717d19da25d31b81ce5b8fcba

tools/otagui/src/components/SingleOTAOptions.vue
@@ -1,0 +1,92 @@
+<template>
+  <form @submit.prevent="sendForm">
+    <FileSelect
+      v-if="input.isIncremental"
+      v-model="incrementalSource"
+      label="Select the source file"
+      :options="targetDetails"
+    />
+    <FileSelect
+      v-model="targetBuild"
+      label="Select a target file"
+      :options="targetDetails"
+    />
+    <OTAOptions
+      :targetDetails="targetDetails"
+      :targetBuilds="[targetBuild]"
+      @update:input="input=$event"
+    />
+    <v-divider class="my-5" />
+    <v-btn
+      block
+      type="submit"
+    >
+      Submit
+    </v-btn>
+  </form>
+</template>
+
+<script>
+import OTAOptions from '@/components/OTAOptions.vue'
+import FileSelect from '@/components/FileSelect.vue'
+import { OTAConfiguration } from '@/services/JobSubmission.js'
+
+export default {
+  components: {
+    OTAOptions,
+    FileSelect,
+  },
+  props: {
+    targetDetails: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      incrementalSource: '',
+      targetBuild: '',
+      input: new OTAConfiguration(),
+    }
+  },
+  computed: {
+    checkIncremental() {
+      return this.input.isIncremental
+    },
+  },
+  watch: {
+    checkIncremental: {
+      handler: function () {
+        this.$emit('update:isIncremental', this.checkIncremental)
+      },
+    },
+  },
+  created() {
+    this.$emit('update:isIncremental', this.checkIncremental)
+    this.$emit('update:Handler', this.setIncrementalSource, this.setTargetBuild)
+  },
+  methods: {
+    /**
+     * Send the configuration to the backend.
+     */
+    async sendForm() {
+      try {
+        let response_message = await this.input.sendForm(
+          this.targetBuild, this.incrementalSource)
+        alert(response_message)
+        this.input.reset()
+      } catch (err) {
+        alert(
+          'Job cannot be started properly for the following reasons: ' + err
+        )
+      }
+    },
+    setIncrementalSource (build) {
+      this.incrementalSource = build
+    },
+    setTargetBuild (build) {
+      this.targetBuild = build
+    }
+  },
+}
+</script>

Project-Mist-OS/development

tools/otagui/src/components/SingleOTAOptions.vue
@@ -1,0 +1,92 @@
+<template>
+  <form @submit.prevent="sendForm">
+    <FileSelect
+      v-if="otaConfig.isIncremental"
+      v-model="incrementalSource"
+      label="Select the source file"
+      :options="targetDetails"
+    />
+    <FileSelect
+      v-model="targetBuild"
+      label="Select a target file"
+      :options="targetDetails"
+    />
+    <OTAOptions
+      :targetDetails="targetDetails"
+      :targetBuilds="[targetBuild]"
+      @update:otaConfig="otaConfig=$event"
+    />
+    <v-divider class="my-5" />
+    <v-btn
+      block
+      type="submit"
+    >
+      Submit
+    </v-btn>
+  </form>
+</template>
+
+<script>
+import OTAOptions from '@/components/OTAOptions.vue'
+import FileSelect from '@/components/FileSelect.vue'
+import { OTAConfiguration } from '@/services/JobSubmission.js'
+
+export default {
+  components: {
+    OTAOptions,
+    FileSelect,
+  },
+  props: {
+    targetDetails: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      incrementalSource: '',
+      targetBuild: '',
+      otaConfig: new OTAConfiguration(),
+    }
+  },
+  computed: {
+    checkIncremental() {
+      return this.otaConfig.isIncremental
+    },
+  },
+  watch: {
+    checkIncremental: {
+      handler: function () {
+        this.$emit('update:isIncremental', this.checkIncremental)
+      },
+    },
+  },
+  created() {
+    this.$emit('update:isIncremental', this.checkIncremental)
+    this.$emit('update:handler', this.setIncrementalSource, this.setTargetBuild)
+  },
+  methods: {
+    /**
+     * Send the configuration to the backend.
+     */
+    async sendForm() {
+      try {
+        let response_message = await this.otaConfig.sendForm(
+          this.targetBuild, this.incrementalSource)
+        alert(response_message)
+        this.otaConfig.reset()
+      } catch (err) {
+        alert(
+          'Job cannot be started properly for the following reasons: ' + err
+        )
+      }
+    },
+    setIncrementalSource (build) {
+      this.incrementalSource = build
+    },
+    setTargetBuild (build) {
+      this.targetBuild = build
+    }
+  },
+}
+</script>